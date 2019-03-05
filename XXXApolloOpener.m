45a06a5f3a40c9f74297f5159c09d0bddd29c2d0
Ken Pham <superhb12@gmail.com>
2019-03-05 11:41:58 -0500

    Update 1.0.2
    - AMP support (should be working now)

--- XXXApolloOpener.m
+++ XXXApolloOpener.m
@@ -13,29 +13,35 @@
     return self;
 }
 
+// youtube doesn't open into app (fix: check for youtube link and change then to youtube://[ID])
+// youtu.be/
 - (id) openURL:(NSURL *)url sender:(NSString *)sender {
     if ([url.host isEqualToString:@"www.reddit.com"] ||
         [url.host isEqualToString:@"reddit.com"] ||
         [url.host isEqualToString:@"m.reddit.com"] ||
         [url.host isEqualToString:@"old.reddit.com"] ||
-        [url.host containsString:@".reddit.com"]) {
+        [url.host containsString:@".reddit.com"] ||
+        [url.path containsString:@"amp.reddit.com"]) {
         
         if ([url.path isEqualToString:@"/"] || [url.path isEqualToString:@""]) {
             return [NSURL URLWithString:@"apollo://"];
+        } else if ([url.path containsString:@"amp.reddit.com"]) { // -- v1.0.2 - OK now AMP is supported --
+            NSString *path = [url.path stringByRemovingPercentEncoding];
+            NSString *ampUrl = [path stringByReplacingOccurrencesOfString:@"/amp/s/amp." withString:@"apollo://"];
+            return [NSURL URLWithString:ampUrl];
         } else {
+            // narwhal://open-url/%@
             return [NSURL URLWithString:[NSString stringWithFormat:@"apollo://reddit.com%@/?%@", url.path, url.query]];
         }
-    } else if ([url.path containsString:@"amp.reddit.com"]) { // -- v1.0.1 - Actually added amp support --
-        NSString *path = [url.path stringByRemovingPercentEncoding];
-        NSString *ampUrl = [path stringByReplacingOccurrencesOfString:@"amp/s/amp." withString:@"apollo://"];
-        return [NSURL URLWithString:ampUrl];
     }
 
-    // -- v1.0.1 - Made it so other links actuall opened
-    NSString *decoded = [url.absoluteString stringByRemovingPercentEncoding];
-    NSString *trimmed = [decoded stringByReplacingOccurrencesOfString:@"http://opener.hbang.ws/_opener_app_link_hax_?original=" withString:@""];
-    NSString *converted = [trimmed stringByReplacingOccurrencesOfString:@" " withString:@""];
-    return [NSURL URLWithString:converted];
+    if ([url.host isEqualToString:@"opener.hbang.ws"]) {
+        NSString *decoded = [url.query stringByRemovingPercentEncoding];
+        NSString *trimmed = [decoded stringByReplacingOccurrencesOfString:@"original=" withString:@""];
+        NSString *converted = [trimmed stringByReplacingOccurrencesOfString:@" " withString:@""];
+        return [NSURL URLWithString:converted];
+    }
+    return url;
 }
 
 @end
